157ae92e4bb085abad0f763e52d33b15d7742771
catherine <vuthi.esther@gmail.com>
2022-07-11 16:22:07 +0300

    final result

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+
+  function handleClick(){
+    setPeople([])
+  }
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={handleClick}>{people.length > 1 ? "Clear all" : "No birthday today"}</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
