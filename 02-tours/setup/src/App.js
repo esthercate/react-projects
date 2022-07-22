7c871b0339eb0659b7bcf82520b124e632aaae30
catherine <vuthi.esther@gmail.com>
2022-07-22 23:29:50 +0300

    adds state and usestate

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,32 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((tours) => {
+      setTours(tours);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App
